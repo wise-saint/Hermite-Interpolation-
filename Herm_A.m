f1d76f03e294355cca4b5591a46825be2a8ae882
9mpd <58243776+9mpd@users.noreply.github.com>
2021-04-27 14:48:26 +0530

    Update Herm_A.m

--- Herm_A.m
+++ Herm_A.m
@@ -1,3 +1,9 @@
+Copyright (c) 2021, 9mpd
+All rights reserved.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+
 function Ai = Herm_A(T, i, N)
 % A supporting function to calculate Hermite interpolating polynomaial
     syms x A(x) Li(x) Diff(x)
